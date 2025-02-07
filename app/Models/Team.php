ad3b9ecf121cfb32ac496d932f5d09154900cb93
Daniel Heinze <danielheinze96@gmail.com>
2025-02-07 14:22:31 +0100

    #3 Games Model Basic

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Database\Factories\TeamFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
@@ -14,4 +16,25 @@ class Team extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function gamesAsHost(): HasMany
+    {
+        return $this->hasMany(Game::class, 'host_id');
+    }
+
+    /**
+     * Get the games where the team is the visitor.
+     */
+    public function gamesAsVisitor(): HasMany
+    {
+        return $this->hasMany(Game::class, 'visitor_id');
+    }
+
+    /**
+     * Get all games where the team is either the host or the visitor.
+     */
+    public function games(): Collection
+    {
+        return $this->gamesAsHost()->union($this->gamesAsVisitor())->get();
+    }
 }
---
+++ database/migrations/2025_02_07_124601_create_games_table.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('host_id')->constrained('teams')->onDelete('cascade');
+            $table->foreignId('visitor_id')->constrained('teams')->onDelete('cascade');
+            $table->integer('host_score')->nullable();
+            $table->integer('visitor_score')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
