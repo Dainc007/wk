3a6a9b9fca8a3357a6bee9e8f569b41490bf0dc0
Daniel Heinze <danielheinze96@gmail.com>
2025-02-07 15:16:19 +0100

    #3 League Model Basic

---
+++ database/factories/LeagueFactory.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\League;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<League>
+ */
+class LeagueFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->country(),
+            'level' => rand(0, 2),
+            'country' => $this->faker->countryCode()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TeamSeeder::class,
             GameSeeder::class,
+            LeagueSeeder::class,
         ]);
 
     }
