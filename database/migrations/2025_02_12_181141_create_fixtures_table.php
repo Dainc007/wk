a76e5a76e59f53ca879322184a2a4c022d102d5c
Daniel Heinze <danielheinze96@gmail.com>
2025-02-13 13:03:11 +0100

    Podstawy + serwis do generowania rundy #5

---
+++ database/migrations/2025_02_12_181141_create_fixtures_table.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fixtures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('host_id')->constrained('teams')->onDelete('cascade');
+            $table->foreignId('visitor_id')->constrained('teams')->onDelete('cascade');
+            $table->date('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fixtures');
+    }
+};
