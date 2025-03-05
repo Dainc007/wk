741379b1e44abb2a5af63730f7facacdea01c6a3
Daniel Heinze <danielheinze96@gmail.com>
2025-03-05 18:59:11 +0100

    Roles & Permissions #27

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,19 +1,13 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Providers;
 
 use App\Models\User;
-use App\Services\LogService;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
-final class AppServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
@@ -28,18 +22,10 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $isProduction = $this->app->isProduction();
+        Vite::prefetch(concurrency: 3);
 
-        Model::shouldBeStrict($isProduction);
-        DB::prohibitDestructiveCommands($isProduction);
-        URL::forceScheme('https');
-
-        date_default_timezone_set(config('app.timezone'));
-
-        LogService::shouldLogMissingTranslationKeys(config('logging.logMissingTranslationKeys'));
-
-        Vite::prefetch(concurrency: 3)->useAggressivePrefetching();
-
-        Gate::define('viewPulse', fn (User $user): bool => $user->isAdmin());
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isAdmin();
+        });
     }
 }
