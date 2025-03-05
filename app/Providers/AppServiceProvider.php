b989a25495ad781ee8a1d64ebf64b8397ea79959
Daniel Heinze <danielheinze96@gmail.com>
2025-03-05 17:30:18 +0100

    refactor: extra setings #27

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use App\Models\User;
 use App\Services\LogService;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,16 +26,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Model::preventLazyLoading(! $this->app->isProduction());
-        Model::preventSilentlyDiscardingAttributes();
-        Model::preventAccessingMissingAttributes();
+        $isProduction = $this->app->isProduction();
+
+        Model::shouldBeStrict($isProduction);
+        DB::prohibitDestructiveCommands($isProduction);
+        URL::forceScheme('https');
+
+        date_default_timezone_set(config('app.timezone'));
 
         LogService::shouldLogMissingTranslationKeys(config('logging.logMissingTranslationKeys'));
 
-        Vite::prefetch(concurrency: 3);
+        Vite::prefetch(concurrency: 3)->useAggressivePrefetching();
 
-        Gate::define('viewPulse', function (User $user) {
-            return $user->isAdmin();
-        });
+        Gate::define('viewPulse', fn(User $user) => $user->isAdmin());
     }
 }
