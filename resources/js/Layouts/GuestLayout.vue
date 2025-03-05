8bfbe34bd77019a21749240f9c7438cbe8105873
Daniel Heinze <danielheinze96@gmail.com>
2025-03-05 20:45:13 +0100

    #29

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,16 +1,29 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
+import NavLink from '@/Components/NavLink.vue';
+
 </script>
 
 <template>
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900"
     >
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
+        <div class="flex space-x-4">
+            <NavLink
+                :class="['bg-white', 'text-blue-600', 'font-semibold', 'py-2', 'px-4', 'rounded', 'shadow', 'transition', 'duration-300', 'hover:bg-blue-100', { 'bg-blue-200': route().current('login') }]"
+                :href="route('login')"
+                :active="route().current('login')"
+            >
+                Login
+            </NavLink>
+            <NavLink
+                :class="['bg-white', 'text-blue-600', 'font-semibold', 'py-2', 'px-4', 'rounded', 'shadow', 'transition', 'duration-300', 'hover:bg-blue-100', { 'bg-blue-200': route().current('register') }]"
+                :href="route('register')"
+                :active="route().current('register')"
+            >
+                Register
+            </NavLink>
         </div>
 
         <div
