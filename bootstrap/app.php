74fbe3a83c56edfc3291870017ac0acc60f10755
Daniel Heinze <danielheinze96@gmail.com>
2025-05-01 15:30:09 +0200

    #36 Cookie-consent

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Spatie\CookieConsent\CookieConsentMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
             LogUserActivity::class,
+            CookieConsentMiddleware::class,
         ]);
 
         //
---
+++ config/filament-cookie-consent.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // 'start', 'end'
+    'position' => 'start',
+
+    'consent_button' => [
+        'size' => 'sm',
+        'color' => 'warning',
+    ],
+
+    'privacy_policy_button' => [
+        'enabled' => true,
+        'href' => '/privacy-policy',
+        'size' => 'sm',
+        'color' => 'gray',
+        'target' => '_blank',
+    ],
+];
